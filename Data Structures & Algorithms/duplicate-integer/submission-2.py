ce80e4926cd754fd1f1f9ed2d5b1bdc85d85185d
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-04-07 16:02:52 -0700

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # Keyword : Search in Rotated Sorted Array -> Binary Search :  Search in Rotated Sorted Array 
+        # Image : Imagine an instant-lookup Map where you check if a Key or Value exists before , if not add it into map
+        # 3-Step Flow
+        #Step 1:   Initialize a map, store the key-value pair
+        seenMap = {}
+        #Step 2:  Traverse the Array to  check if a  a Key or Value exists before, if not add it into map
+        for num in nums:
+            if num in seenMap:
+                return True
+            seenMap[num] = True
+        #Step3: Return the result
+        return False
+
+# Time complexity: O(N) ... Traverse size N Array
+# Space complexity:  O(N)....create size N Array
+def test():
+    sol = Solution()
