e0fc966fd4388e7794cc3e385d06c91d6b54e7fe
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-04-24 14:47:53 -0500

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.py
@@ -0,0 +1,36 @@
+"""
+ OOD: No
+ Constraints: No
+ input : str
+ output : boolean
+"""
+# Keywork : Two Sum -> but solution must use O(1) additional space -> cannot use hashmap
+# Keyword : “Palindrome", "Target Sum"  -> Basic Two pointer 
+# Image : Two pointer Shrink from both ends to find the perfect fit
+# Tricks:
+    # Sorting is a great way to prep your data before applying a two-pointer approach.
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers)-1
+        while l < r:
+            curSum = numbers[l] + numbers[r]
+            if curSum < target:
+                l+=1
+            elif curSum > target:
+                r-=1
+            else:
+                return [l+1,r+1]
+        return []
+
+
+def test():
+    sol = Solution()
+    result = sol.twoSum([1,2,3,4],3)
+    print(f"Result: {result})")
+if __name__ == "__main__":
+    test()
+
+# Time complexity: O(N) ...traverse size N array
+# Space complexity:  O(1)....create constant variable
