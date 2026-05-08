1412e1b10fc1c7b74c7794a9d03eb98372b01fe6
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-05-08 17:05:26 -0400

    Add: daily-temperatures - submission-8

---
+++ Data Structures & Algorithms/daily-temperatures/submission-8.py
@@ -0,0 +1,41 @@
+"""
+ OOD: No
+ Constraints: No
+ input : List[int]
+ output : List[int]
+"""
+# Keyword : “Next greater or smaller element" -> monotonic 
+# Image: Traverse the array
+# 1.push new element into monotonic decreasing stack bc the smaller cannot save the current element but it could be saved by future element.
+# 2.pop the index while stack  is not empty and greater element come save and record the gap
+
+from typing import List
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0] * len(temperatures)
+        for i, t in enumerate(temperatures):
+            while stack and t > temperatures[stack[-1]]:
+                prev_idx = stack.pop()
+                gap = i - prev_idx
+                res[prev_idx] = gap
+            stack.append(i)
+        return res
+
+def test():
+    sol = Solution()
+    temperatures = [30,38,30,36,35,40,28]
+    result = sol.dailyTemperatures(temperatures)
+    print(f"Result:{result}")
+
+if __name__ == "__main__":
+    test()
+
+# Time complexity: O(N) ... Traverse size N Array
+# Space complexity:  O(N)....create size N Stack  & size N result array
+
+
+
+
+
+
