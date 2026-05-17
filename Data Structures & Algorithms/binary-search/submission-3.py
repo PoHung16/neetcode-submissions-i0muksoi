cef9186b5cb990a7ddfbc362ad49598283b46f26
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-05-17 17:06:20 -0400

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.py
@@ -0,0 +1,45 @@
+"""
+ OOD: No
+ Constraints: Yes
+ input : List[int],int
+ output : int
+"""
+# Keyword : “Sorted Array " or "Sorted 2D martix" -> Basic Binary Search
+# Image :  Traverse the array with l, r pointer, and we compare the mid value with target, if equal we find it, it target is smaller, we search left side, if target is larger, we search the right side
+# Tricks :  
+    # use “=”, "l <= r” ensures the loop still runs when the search range shrinks to a single element.
+
+from typing import List
+class Solution:
+    def search(self, nums:List[int],target:int) -> int:
+        l, r = 0, len(nums)-1
+        while l<=r :
+            mid = (l+r)//2
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                r = mid -1
+            else:
+                l = mid +1
+        return -1
+def test():
+    sol = Solution()
+    nums = [-1,0,2,4,6,8]
+    target= 4
+    result = sol.search(nums,target)
+    print(f"Result:{result}")
+
+if __name__ == "__main__":
+    test()
+    
+
+# Time Complexity: O(logN) .... Since its divided by h times, 2^h = n, h = logN
+# Space Complexity: O(1)... We didn't create extra variable or data structure       
+
+
+
+
+        
+
+        
+        
