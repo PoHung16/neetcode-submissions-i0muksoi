a9aede9b9510ab808ba8d5165ab74d89543e8c0c
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-06-15 15:08:16 -0400

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.py
@@ -0,0 +1,67 @@
+"""
+ OOD: No
+ Constraints: No
+ input : List[int]
+ output : int , Single Number (max/min profit, cost, ways, jumps)-> DP
+"""
+# Keyword :  “max/min profit, cost, ways, jumps” or fibonacci sequence ->  Bottom up DP Tabulation - edge cae
+# Image :  DP is remembering past results to build the next one, I'll init the base case, loop through with the transitions formula, and return the final result
+# Tricks:
+   # If the current state only depends on the last one or two steps, you can ditch the dp array and just use dp variables forward to achieve O(1) space
+
+class Solution:
+    def maxProfitArray(self, prices: List[int]) -> int:
+        # edge case
+        if not prices:
+            return 0
+        if len(prices)<2:
+            return 0
+        # base case
+        dp = [0] * n
+        min_price = prices[0]
+        # loop through with the transitions formula
+        for i in range(1,n):
+            min_price = min(min_price, price[i])  # Update the lowest price seen so far
+            dp[i] = max(dp[i-1], prices[i]- min_price) #Max profit is either previous max OR (current - min)
+        return dp[-1]
+    
+    # Time complexity: O(N) ... Traverse size N Array
+    # Space complexity:  O(N)....create size N Array
+
+    def maxProfit(self, prices: List[int]) -> int:
+        # edge case
+        if not prices:
+            return 0
+        if len(prices)<2:
+            return 0
+        # base case
+        min_price = prices[0]
+        dp_maxium_profit = 0
+        # loop through with the transitions formula
+        for i in range(1, len(prices)):
+            min_price = min(min_price, prices[i])
+            dp_maxium_profit = max(dp_maxium_profit, prices[i]-min_price)
+        return dp_maxium_profit
+    
+    # Time Complexity: O(n)....traverse size array
+    # Space Complexity: O(1).... constant space 
+
+def test():
+    sol = Solution()
+    result = sol.maxProfit([10,1,5,6,7,1])
+    print(f"Result : {result}")
+
+if __name__ == "__main__":
+    test()
+
+
+
+
+       
+        
+        
+      
+            
+        
+        
+
