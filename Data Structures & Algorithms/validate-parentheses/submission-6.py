ac4b8553e31cc46c53682a20febe0e3f8ddedeb5
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-06-15 21:16:26 -0400

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,39 @@
+"""
+ OOD: No
+ Constraints: No
+ input : String
+ output : boolean
+"""
+# Keyword : “Parentheses","Reverse Polish Notation" -> Basic Stack
+# Image : Map the pairs, then traverse the string: push the lefts, and for every right, pop the top if stack is not empty and stack top is a match
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closeToOpen = {
+            ")" : "(",
+            "]" : "[",
+            "}" : "{",
+        }
+        stack = []
+        for char in s:
+            if char not in closeToOpen:
+                stack.append(char)
+            else:
+                if stack and stack[-1] == closeToOpen[char]:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack)==0 # means every left have a match
+
+# Time complexity: O(N) ... Traverse size N Array
+# Space complexity:  O(N)....create size N Stack  & Size 3 Map
+
+def test():
+    sol = Solution()
+    nums = s = "[]"
+    result = sol.isValid(s)
+    print(f"Result:{result}")
+
+if __name__ == "__main__":
+    test()
+
+
