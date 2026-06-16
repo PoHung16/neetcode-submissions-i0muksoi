dddf38c83a4048b1097e30cf0fc40cebd9481e34
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-06-15 21:30:52 -0400

    Add: evaluate-reverse-polish-notation - submission-6

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-6.py
@@ -0,0 +1,45 @@
+"""
+ OOD: No
+ Constraints: No
+ input : List[String]
+ output : integer
+"""
+# Keyword : “Parentheses","Reverse Polish Notation" -> Basic Stack
+# Image : Map the pairs, then traverse the string: push the lefts, and for every right, pop the top to if stack is not empty and stack top is a match
+# Image : Map the Operation, then traverse the string: push the numbers, and for every operator, pop two, calculate, and push the result back."
+
+from typing import List
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        operations = {
+            "+" : lambda a,b : a+b,
+            "-" : lambda a,b : a-b,
+            "*" : lambda a,b : a*b,
+            "/" : lambda a,b : int(a/b),
+
+        }
+        stack = []
+        for token in tokens:
+            if token not in operations:
+                stack.append(int(token))
+            else:
+                b = stack.pop()
+                a = stack.pop()
+                result = operations[token](a,b)
+                stack.append(result)
+        return stack[-1] #stack[0]也可以
+
+# Time complexity: O(N) ... Traverse size N Array
+# Space complexity:  O(N)....create size N Stack  & Size 4 Map
+
+
+def test():
+    sol = Solution()
+    tokens = ["1","2","+","3","*","4","-"]
+    result = sol.evalRPN(tokens)
+    print(f"Result:{result}")
+
+if __name__ == "__main__":
+    test()
+
+
