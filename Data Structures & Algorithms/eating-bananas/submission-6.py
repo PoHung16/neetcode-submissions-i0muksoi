c9585c8c4fccf8514b54e5570c803ca3ed65600b
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-06-17 22:44:40 -0400

    Add: eating-bananas - submission-6

---
+++ Data Structures & Algorithms/eating-bananas/submission-6.py
@@ -0,0 +1,45 @@
+"""
+ OOD: No
+ Constraints: No
+ input : List[int],int
+ output : int
+"""
+# Keyword : "Find the minimum or maximum value to pass a test.">  Binary Search on Answer 題型
+# Image :  Traverse the array with l, r pointer
+# Tricks :  
+   # A. The squeeze use "while l <= r” ensures the loop still runs when the search range shrinks to a single element.
+   # B.  put l & r on the lowest and highest possible answers. Use mid as a test answer. If mid passes, try a slower answer on the left. If it fails, look for a larger answer on the right.
+
+from typing import List
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r 
+        while l <= r:
+            mid = (l+r) //2
+            total_time = 0
+            for p in piles:
+                total_time += (p + mid - 1) // mid
+            if total_time <= h:
+                res = mid
+                r = mid -1
+            else:
+                l = mid+1
+        return res
+ 
+# Time Complexity: O(NlogM) .... M is the search range on the answer,  N is the total number of piles in the input array.
+    # Everytime we perform binary search, we will need to traverse size N array to calculate hours_needed
+# Space Complexity: O(1)... We didn't create extra variable or data structure       
+
+
+def test():
+    sol = Solution()
+    piles = [1,4,3,2]
+    h = 9
+    result = sol.minEatingSpeed(piles,h)
+    print(f"Result:{result}")
+
+if __name__ == "__main__":
+    test()
+
+
