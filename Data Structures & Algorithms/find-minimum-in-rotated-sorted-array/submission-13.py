3aeb86e5c5f6eb0c9ddfb5b3e37ed6a14fa05e24
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-06-17 22:27:47 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-13

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-13.py
@@ -0,0 +1,48 @@
+
+"""
+ OOD: No
+ Constraints: Yes
+ input : List[int],int
+ output : int
+"""
+# Keyword : “Sorted Array " or "Sorted 2D matrix" or "Search in rotated array" -> Basic Binary Search
+# Image :  Traverse the array with l, r pointer
+# Tricks :  
+    # Common Situation: 
+        # A. The squeeze use "while l <= r” ensures the loop still runs when the search range shrinks to a single element.
+        # B. We compare the mid value with target, if equal we find it, it target is smaller, we search left side, if target is larger, we search the right side
+    # 2D Array Situation
+        # row = mid // n , col = mid % n 
+    # Search in Rotated Sorted Array Situation
+        # A. Check if the LEFT side is perfectly sorted  (Compare nums[left] with nums[mid])
+        # B. keep performing binary search on sorted side
+   # Find "minimum" in Rotated Sorted Array Situation:
+        # A. Squeeze range using "while l < r": The loop stops automatically when only one element is left (the minimum).
+        # B. Check if the RIGHT side is perfectly sorted (Compare  nums[mid] with nums[r])
+        # C. Shrink: If nums[mid] > nums[r], skip mid (l = mid + 1). Otherwise, keep mid (r = mid).
+
+
+from typing import List
+class Solution:
+    def findMin(self, nums:List[int]) -> int:
+        l, r = 0, len(nums)-1
+        while l < r:
+            mid = (l+r) //2
+            if nums[mid] > nums[r] : #minimum is at the right side
+                l = mid+1
+            else:
+                r = mid # mininum is at the left side , including mid
+        return nums[l]
+
+
+# Time complexity: O(LogN) ... Binary Search...Tree height
+# Space complexity:  O(1)....create constant variable
+def test():
+    sol = Solution()
+    nums = [-1,0,2,4,6,8]
+    result = sol.findMin(nums)
+    print(f"Result:{result}")
+
+if __name__ == "__main__":
+    test()
+
