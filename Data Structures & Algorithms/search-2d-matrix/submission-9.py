0cdc408b1e38d5f00dd9a9b4522f0ea046afae94
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-06-17 22:12:00 -0400

    Add: search-2d-matrix - submission-9

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-9.py
@@ -0,0 +1,47 @@
+"""
+ OOD: No
+ Constraints: Yes
+ input : List[List[int]],int
+ output : boolean
+"""
+# Keyword : “Sorted Array " or "Sorted 2D matrix" or "Search in rotated array" -> Basic Binary Search
+# Image :  Traverse the array with l, r pointer
+# Tricks :  
+    # Common Situation: 
+        # A. The squeeze use "while l <= r” ensures the loop still runs when the search range shrinks to a single element.
+        # B. We compare the mid value with target, if equal we find it, it target is smaller, we search left side, if target is larger, we search the right side
+    # 2D Array Situation
+        # row = mid // n , col = mid % n 
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target:int)->bool:
+        m, n = len(matrix), len(matrix[0])
+        l ,r = 0, m*n-1
+        while l <= r:
+            mid = (l+r) //2
+            row = mid // n
+            col = mid % n
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                l = mid + 1
+            else:
+                r = mid -1
+        return False
+
+def test():
+    sol = Solution()
+    matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]]
+    target= 4
+    result = sol.searchMatrix(matrix,target)
+    print(f"Result:{result}")
+
+if __name__ == "__main__":
+    test()
+
+# Time Complexity: O(logM*N) .... Since its divided by h times, 2^h = n, h = logN
+# Space Complexity: O(1)... We didn't create extra variable or data structure       
+
+
+
+
