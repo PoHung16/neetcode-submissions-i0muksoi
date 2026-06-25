16a85fec7348bcd273ddf709bb1167b9c685e4ef
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-06-25 19:40:31 -0400

    Add: k-closest-points-to-origin - submission-4

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-4.py
@@ -0,0 +1,54 @@
+"""
+ OOD: No
+ Constraints: No
+ input : List[List[int]], int
+ output :  List[List[int]]
+"""
+# Keyword: Top K elements -> Max/Min Heap
+# Image :build a heap that only holds K spots traverse the array, and the weakest/biggest ones get kicked out the top, where new elements "bubble" into place
+from typing import List
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k:int) -> List[List[int]]:
+      maxheap = []
+      for point in points:
+        x, y = point[0],point[1]
+        dist = x**2 + y**2
+        heapq.heappush(maxheap,(-dist,x,y))
+        if len(maxheap)>k:
+          heapq.heappop(maxheap)
+      res = []
+      while len(maxheap)>0:
+        dist,x,y = heapq.heappop(maxheap)
+        res.append([x,y])
+      return res
+
+def test():
+  sol = Solution()
+  points = [[0,2],[2,0],[2,2]]
+  result = sol.kClosest(points,2)
+  print(f"result: {result}")
+
+if __name__ == "__main__":
+    test()
+
+# Time Complexity: O(N log K)
+  # We process N points, and each heap operation takes log K time. O(NlogK)
+  # while maxHeap : O(KlogK).. travser size k heap, and each time we perform heap opeartion O(logK)
+# Space complexut : O(K)
+  # Create siez K heap
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
