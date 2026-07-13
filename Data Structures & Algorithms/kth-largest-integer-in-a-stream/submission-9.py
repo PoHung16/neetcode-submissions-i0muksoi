fe8a3e7486e48cf94e649b8c060c88f91c7b7101
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-07-13 15:01:19 -0400

    Add: kth-largest-integer-in-a-stream - submission-9

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-9.py
@@ -0,0 +1,57 @@
+"""
+ OOD: Yes
+ Constraints: No
+ input : constructor/method
+ output : constructor/method
+"""
+# A. Clarify the goal: find the $kth largest element in the stream
+# B. Decide the data strucure
+    # Keyword: Top K elements -> Max/Min Heap
+    # Approach: Traverse an array and  build a heap that only holds K spots, then kick out the smallest one on the top,  where new elements "bubble" into place
+ # C. Implement constructor and method
+
+from typing import List
+import heapq
+class KthLargest:
+    def __init__(self, k:int, nums:List[int]):
+        self.minheap = []
+        self.k = k
+        for num in nums:
+            heapq.heappush(self.minheap,num)
+            if len(self.minheap)>self.k:
+                heapq.heappop(self.minheap)
+    def add(self, val:int)->int:
+        heapq.heappush(self.minheap,val)
+        if len(self.minheap)>self.k:
+            heapq.heappop(self.minheap)
+        return self.minheap[0]
+
+
+# Time Complexity: 
+    # Constructor: O(NlogK)
+        # Traverse size N array to perform heap operation: N次 每次heappop: O(logK)
+    # Add: O(log K) ...push the elment into size K heap
+# Space Complexity: O(K) - we create size K heap
+
+
+
+
+def test():
+    kthLargest = KthLargest(3, [1, 2, 3, 3])
+    result1 = kthLargest.add(3)
+    result2 = kthLargest.add(5)
+    result3 = kthLargest.add(6)
+    result4 = kthLargest.add(7)
+    result5 = kthLargest.add(8)
+    print(f"Result:{result1}") 
+    print(f"Result:{result2}")
+    print(f"Result:{result3}")
+    print(f"Result:{result4}")
+    print(f"Result:{result5}")
+    
+if __name__ == "__main__":
+    test()
+
+
+
+    
