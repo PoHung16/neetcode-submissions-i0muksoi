a3b97fe64cf9be7772aeaccefccf0ed307570bfa
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-07-15 16:11:24 -0400

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.py
@@ -0,0 +1,50 @@
+"""
+ OOD: No
+ Constraints: No
+ input : TreeNode
+ output : List[int]
+"""
+# Optimal Solution
+    # Keyword: Tree Search problem  - "Level by level" -> BFS with a Queue
+    # Approach: base case -> Initialize Queue -> Measure Level Size -> Process Current Level -> Queue Up the Next Level
+
+class TreeNode:
+    def __init__(self,val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from collections import deque
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        # Step 0
+        if not root:
+            return []
+            
+        # 【動作 1: 進】
+        queue = deque([root])
+        res = []
+        
+        while queue:
+            # 【動作 2: 鎖】
+            # 拍照存證：這一層有幾個人
+            level_size = len(queue)
+            
+            # 【動作 3: 辦】
+            for i in range(level_size):
+                node = queue.popleft()
+                
+                # --- 唯一的差別在這裡 ---
+                # 如果我是這一層的最後一個 index (i == n-1)
+                # 代表我站在最右邊 (從右邊只看得到我)
+                if i == level_size - 1:
+                    res.append(node.val)
+                # ---------------------
+                
+                # 依然要把小孩加進去，因為下一層還是要看全部
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                    
+        return res
