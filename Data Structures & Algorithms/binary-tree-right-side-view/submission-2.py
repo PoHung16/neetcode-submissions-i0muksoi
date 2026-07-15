4cfa8ca6b5045b1fd536849ef2fd432328a1ca77
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-07-15 16:21:41 -0400

    Add: binary-tree-right-side-view - submission-2

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-2.py
@@ -0,0 +1,43 @@
+"""
+ OOD: No
+ Constraints: No
+ input : TreeNode
+ output : List[int]
+"""
+# Optimal Solution
+    # Keyword: Tree Search problem  - "Level by level" -> BFS with a Queue
+    # Approach: base case -> Initialize Queue -> Measure Level Size -> Process Current Level -> Queue Up the Next Level
+    # Tricks:
+        # If it's the last node in the current level, it's visible from the right!
+
+class TreeNode:
+    def __init__(self,val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from collections import deque
+class Solution:
+    def rightSideView(self, root: TreeNode)-> List[int]:
+        if not root: 
+            return[]
+        res = []
+        queue = deque([root])
+        while queue:
+            level_size = len(queue)
+            current_level = []
+            for i in range(level_size):
+                node = queue.popleft()
+                if i == level_size-1:
+                    res.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return res
+
+# Time complexity: O(N) ...  Traverse N nodes in a binary tree - one side,
+# Space complexity:  O(N)... the worst case - perfect binary tree, N/2 in bottom level
+ 
+
+
