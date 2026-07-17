0f8dbd5c5241845632560c6b87871c0588c74150
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-07-17 15:54:29 -0400

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.py
@@ -0,0 +1,41 @@
+"""
+ OOD: No
+ Constraints: No
+ input : List[int]
+ output : int
+"""
+# Brute Force: 
+    # Use nested loop to check every possible subarray sum from index i to j -> O(N^2)
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = float("-inf")
+        for i in range(len(nums)):
+            current_sum = 0
+            for j in range(i,len(nums)):
+                current_sum += nums[j]
+                max_sum = max(max_sum, current_sum)
+        return max_sum
+
+# Optimal Solution
+    # Keyword:  "Maximum Subarray", "Contiguous Sum" -> Greedy (Decide optimally at each step using a single state variable to reach the global best)
+    # Approach: Maintain a single running sum variable. At each step, greedily drop the previous sum if it becomes negative. This localized optimal choice directly leads to the global maximum result.
+from typing import List
+class Solution:
+    def maxSubArray(self, nums:[List[int]])->int:
+        max_sum = float("-inf")
+        current_sum = 0 
+        for i in range(len(nums)):
+            current_sum += nums[i]
+            max_sum =  max(max_sum,current_sum)
+            if current_sum < 0:
+                current_sum = 0
+        return max_sum
+
+# Time complexity: O(N) ... Traverse size N
+# Space complexity:  O(1) ... Only constant variable
+
+
+
+
+
