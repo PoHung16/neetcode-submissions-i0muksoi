15bb9d4db265f6b7c5e3cab9f551a1fd94ab5be2
PoHung16 <87808820+PoHung16@users.noreply.github.com>
2026-07-23 19:24:02 -0400

    Add: products-of-array-discluding-self - submission-11

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-11.py
@@ -0,0 +1,50 @@
+"""
+ OOD: No
+ Constraints: No
+ input : List[int]
+ output : List[int]
+"""
+# Brute Force: 
+    # Array - Use nested loops to multiply all other elements for each position except for itself -> O(N^2)
+class Solution:
+    def productExceptSelf(self, nums:[List[int]]) -> List[int]:
+        res = [1] * len(nums)
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i!=j:
+                    res[i] *= nums[j]
+        return res
+                            
+# Optimal Soltion:
+    # Goal : O(N^2) / O(NlogN) -> O(N)
+    # Keyword : “Except Self" -> PrefixProduct
+    # Approach: Use Array to record prefix product and suffix product. One goes from left to right, and the other goes from right to left
+# Jump to "Subarray Sum Equals K problem"  
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * len(nums)
+        prefixProduct = 1
+        for i in range(len(nums)):
+            res[i] = prefixProduct
+            prefixProduct *= nums[i]
+        suffixProduct = 1
+        for j in range(len(nums)-1,-1,-1):
+            res[j] *= suffixProduct
+            suffixProduct *= nums[j]
+        return res
+# Time complexity: O(N) ... Traverse size N Array *2
+# Space complexity:  O(1)....In place
+def test():
+    sol = Solution()
+    nums = [1, 2, 3, 3]
+    result = sol.productExceptSelf(nums)
+    print(f"result:{result}")
+if __name__ == "__main__":
+    test()
+
+
+
+
+ 
+
+
